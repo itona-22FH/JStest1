389799ad86b4193065b6a968a02e52a6b540ca01
itona-22FH <naov7700@gmail.com>
2022-02-11 00:19:31 +0900

    JavaScript課題1-3修正

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 'use strict';
 
-function outBorder() {
-  console.log('========================');
-}
-const getOut = '終了';
-
 const tasks = ['掃除', '買い物', '散歩'];
-let task;
-while (task !== getOut) {
-  outBorder();
+let addTask;
+
+const title = () => {
+  console.log('========================');
   console.log('現在持っているタスク一覧');
-  outBorder();
-  for (let i = 0; i < tasks.length; i++) {
-    console.log(`${i} : ${tasks[i]}`);
-  }
-  task = prompt('タスクを入力してください。');
-  if (task !== getOut) {
-    alert('新しいタスクを追加しました。');
-  } else if (task === getOut) {
-    break;
-  }
-  tasks.push(task);
-}
+  console.log('========================');
+};
+
+const arryToProcess = (arry) => {
+  arry.forEach((value, index) => {
+    console.log(`${index} : ${value}`);
+  });
+};
+
+title();
+arryToProcess(tasks);
+
+let inputTask = prompt('タスクを入力してください');
+tasks.push(inputTask);
+alert('タスクを追加しました');
+
+title();
+arryToProcess(tasks);
