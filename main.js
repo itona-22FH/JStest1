38c61d86077475245eaf3868a9db026f6da30a8d
itona-22FH <naov7700@gmail.com>
2022-02-13 17:16:51 +0900

    JavaScript課題1-3修正版-2

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
 'use strict';
 
-const tasks = [
-  { task: '机を片付ける', genre: '掃除' },
-  { task: '牛乳を買う', genre: '買い物' },
-  { task: '散歩する', genre: '運動' },
-];
+const tasks = ['掃除', '買い物', '散歩'];
 
 const title =
   '========================\n現在持っているタスク一覧\n========================';
 
 const showTasks = () => {
   tasks.forEach((value, index) => {
-    console.log(`${index} : [内容]${value.task}、[ジャンル]${value.genre}`);
+    console.log(`${index} : ${value}`);
   });
 };
 
 console.log(title);
-showTasks();
+showTasks(tasks);
 
 let inputTask = prompt('タスクを入力してください');
-let inputGenre = prompt('ジャンルを入力してください');
-tasks.push({ task: inputTask, genre: inputGenre });
+tasks.push(inputTask);
 alert('タスクを追加しました');
 
 console.log(title);
-showTasks();
+showTasks(tasks);
